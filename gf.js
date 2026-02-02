27e20dab74151e565a305068dff840b6085463f8
Yatin Vajja <yvajja.2004@gmail.com>
2026-02-01 23:27:58 -0600

    update

---
+++ gf.js
@@ -0,0 +1,14 @@
+const noBtn = document.getElementById("noBtn");
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * (window.innerWidth - 100);
+  const y = Math.random() * (window.innerHeight - 100);
+  noBtn.style.position = "absolute";
+  noBtn.style.left = x + "px";
+  noBtn.style.top = y + "px";
+});
+
+function yesClicked() {
+  document.getElementById("response").innerText =
+    "YAYYY 💖 I knew you'd say yes 😌";
+}
